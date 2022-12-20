f7e6052d933bcab07b2eee8f721d415536a7c30a
priya6979 <bijaypriya5@gmail.com>
2022-12-20 07:53:58 +0530

    Added Login part

--- src/App.js
+++ src/App.js
@@ -1,49 +1,94 @@
-import axios from 'axios'
+import axios from 'axios';
 import { useEffect, useState } from 'react';
 import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import ImageCard from './components/ImageCard';
 import ImageSearch from './components/ImageSearch';
-import './styles.css'
+import './styles.css';
 
 function App() {
-  const[images,setImages]=useState([])
-  const[isloading,setIsloading]=useState(true)
-  const[term,setTerm]=useState("cat")
+	const [images, setImages] = useState([]);
+	const [isloading, setIsloading] = useState(true);
+	const [term, setTerm] = useState('cat');
+	const [login, setLogin] = useState(false);
+	const [username, setUserName] = useState('');
+	const [password, setPassword] = useState('');
 
-  useEffect(()=>{
-    axios.get(`https://pixabay.com/api/?key=29536220-90609b5fadda99b9a0981664f&q=${term}&image_type=photo&pretty=true`)
-    .then(res=>{
-      setImages(res.data)
-      setIsloading(false)
-    })
-    .catch(error=>{
-      console.log('Error',error)
-    })
+	const clickHandler = () => {
+		setLogin(!login);
+		localStorage.setItem('Username', username);
+		localStorage.setItem('password', password);
+	};
 
-  },[term])
-  return (
-    <div className="App">
-      <Header/>
-      <ImageSearch searchText={text => setTerm(text)}/>
-      {!isloading && images.hits.length===0 && <h1 className='message'>No Images Found</h1>}
-      <div className='flex-main'>
-         {isloading ? <h1>Loading...</h1>:( 
-      images && images?.hits?.map((image)=>{
-        return(
-          <div className='main'>  <ImageCard key={image.id} image={image} /></div>
-        
-         
-      )
-      })
-     )}</div>
-     
-    
+	useEffect(() => {
+		axios
+			.get(
+				`https://pixabay.com/api/?key=29536220-90609b5fadda99b9a0981664f&q=${term}&image_type=photo&pretty=true`
+			)
+			.then((res) => {
+				setImages(res.data);
+				setIsloading(false);
+			})
+			.catch((error) => {
+				console.log('Error', error);
+			});
+	}, [term]);
+	return (
+		<div className="App">
+			<Header />
 
-     <Footer/>
-    </div>
-  );
+			{!login ? (
+				<div className="login">
+					<div className="form-card">
+						<h1>Login</h1>
+						<input
+							className="field"
+							placeholder="Username...xyz@gmail.com"
+							type="text"
+							onChange={(e) => setUserName(e.target.value)}
+						/>
+						<input
+							className="field"
+							placeholder="Password..."
+							type="text"
+							onChange={(e) => setPassword(e.target.value)}
+						/>
+						<button onClick={clickHandler}>Submit</button>
+					</div>
+				</div>
+			) : (
+				<div>
+					<ImageSearch searchText={(text) => setTerm(text)} />
+					{!isloading && images.hits.length === 0 && (
+						<h1 className="message">No Images Found</h1>
+					)}
+					<div className="flex-main">
+						{isloading ? (
+							<h1>Loading...</h1>
+						) : (
+							images &&
+							images?.hits?.map((image, index) => {
+								return (
+									<div
+										key={index}
+										className="main"
+									>
+										<ImageCard
+											key={image.id}
+											image={image}
+										/>
+									</div>
+								);
+							})
+						)}
+					</div>
+				</div>
+			)}
+
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,39 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const ImageCard = ({image}) => {
+const ImageCard = ({ image }) => {
+	const tags = image.tags.split(',');
 
-    const tags= image.tags.split(',');
+	return (
+		<div className="card">
+			<img
+				src={image.webformatURL}
+				alt="loading.."
+			></img>
+			<div>
+				<Link
+					image={image}
+					to="/product"
+				>
+					<h3 className="main">Photo by {image.user}</h3>
+				</Link>
 
-  return (
-    <div className='card'> <img src={image.webformatURL} alt='loading..'></img>
-    <div ><h3  className='main'>Photo by {image.user}</h3>
-    
-        <ul >
-        <li>
-        <strong>Views: </strong>
-        {image.views}
-        </li>
-        <li>
-            <strong>Downloads: </strong>
-            {image.downloads}
-        </li>
-        <li>
-            <strong>Likes: </strong>
-            {image.likes}
-        </li>
-        </ul>
-    </div>
-    <div className='tags'>
-        {tags.map((tag,index)=>(
-            <span key={index} className='tags1'>#{tag}</span>
-        ))}
-
-    </div>
-    </div>
-    
-    
-    
-  )
-}
+				<ul>
+					<li>
+						<strong>Views: </strong>
+						{image.views}
+					</li>
+					<li>
+						<strong>Downloads: </strong>
+						{image.downloads}
+					</li>
+					<li>
+						<strong>Likes: </strong>
+						{image.likes}
+					</li>
+				</ul>
+			</div>
+			<div className="tags">
+				{tags.map((tag, index) => (
+					<span
+						key={index}
+						className="tags1"
+					>
+						#{tag}
+					</span>
+				))}
+			</div>
+		</div>
+	);
+};
 
 export default ImageCard;
